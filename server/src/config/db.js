7c8e3aea1f7faf9c37ca43157de2060aa5e94c5f
arjitha10 <231401014@rajalakshmi.edu.in>
2025-09-18 21:59:46 +0530

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,10 +2,7 @@
 import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI not set in environment variables");
-  }
-
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI not set");
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
