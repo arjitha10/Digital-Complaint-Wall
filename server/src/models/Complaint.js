ba992bc6881d11071ded3cb27fbfabf5e523c35f
arjitha10 <231401014@rajalakshmi.edu.in>
2025-09-18 22:02:02 +0530

    Update Complaint.js

--- server/src/models/Complaint.js
+++ server/src/models/Complaint.js
@@ -1,14 +1,18 @@
+// src/models/Complaint.js
 import mongoose from "mongoose";
 
-const complaintSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  status: { type: String, default: "pending" },
-  createdAt: { type: Date, default: Date.now },
-});
+const complaintSchema = new mongoose.Schema(
+  {
+    complaintNumber: String,
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    category: String,
+    description: String,
+    priority: { type: String, enum: ["Low", "Medium", "High"] },
+    status: String,
+    adminNote: String,
+    contactEmail: String,
+  },
+  { timestamps: true }
+);
 
-const Complaint = mongoose.model("Complaint", complaintSchema);
-
-export default Complaint;
-
-
+export default mongoose.model("Complaint", complaintSchema);
